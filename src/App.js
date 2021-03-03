1ba2ebd72eb527e834611f2c825403305bd176e3
Andrei <andrei.girip99@gmail.com>
2021-03-03 19:08:58 +0000

    test

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth,API,graphqlOperation } from 'aws-amplify';
-import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react';
 import aws_exports from './aws-exports';
 import {listScoress} from './graphql/queries';
 import {createScores} from './graphql/mutations'
+import ArrowRight from './images/ArrowRight.jpg'
+import ArrowLeft from './images/ArrowLeft.jpg'
+import Plus from './images/plus.jpg'
+import Star from './images/star.jpg'
 Amplify.configure(aws_exports);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.submitForm = this.submitForm.bind(this);
+    this.test1MainPage = this.test1MainPage.bind(this);
+    this.getuserscore = this.getuserscore.bind(this);
+    this.nextInstruction = this.nextInstruction.bind(this);
+    this.lastInstruction= this.lastInstruction.bind(this);
+    
+}
   componentDidMount = () => {
     this.getuserscore()
+    this.setState({height: window.innerHeight + 'px'});
+    this.setState({width: window.innerWidth + 'px'});
   }
     state={
       testStarted : false,
       userscores :  [],
       username :Auth.user.username,
-      display : false
+      display : false,
+      width: undefined,
+      height: undefined,
+      instructionPageTest1 : 1
     }
   async createScoresf(dictionary){
     try {
@@ -48,15 +66,92 @@ class App extends Component {
    submitForm(){
     this.setState({testStarted : true})
    }
+   test1MainPage(){
+     this.setState({testStarted :false})
+   }
+   nextInstruction(){
+    this.setState({instructionPageTest1 : this.state.instructionPageTest1 +1 })
+   }
+   lastInstruction(){
+     this.setState({instructionPageTest1 : this.state.instructionPageTest1 -1 })
+   }
   render() {
     if(this.state.display){
       return(
         <div>Here I will show the graph</div>
        )
     }else if(this.state.testStarted){
-      return(
-        <div>Here will be the test</div>
-      )
+      switch(this.state.instructionPageTest1){
+        case 1:{
+              return(
+                  <div> 
+                    <div id="instructionPage1" className="instructionsPage"  >
+                      <p>This test measures some aspects of attention, and takes about 10 minutes to complete.</p>
+                      <p>You will see 5 <b><span className='targetTypeRef'>target</span>s</b> on the computer screen</p>
+                      <center>
+                      <img src={ArrowRight} alt="right Arrow" /><img src={(ArrowRight)} alt="right Arrow" /><img src={ArrowRight}  alt="right Arrow" /><img src={ArrowRight} alt="right Arrow" /><img src={ArrowRight} alt="right Arrow" />
+                      </center>
+                      <p>You must pay attention to the <b><i>CENTRAL</i></b> <span className='targetTypeRef'>target</span>, and indicate which way it is pointing by pressing the LEFT or RIGHT arrow keys on the keyboard.
+                      </p>
+                      <input className="previousButton" type="button" name="previous" value="Previous" onClick={this.test1MainPage} />
+                      <input className="nextButton" type="button" name="next" value="Next" onClick={this.nextInstruction}/>
+                  </div>
+                </div>
+              )
+            }
+        case 2:{
+          return(
+            <div id="instructionPage2" className="instructionsPage">
+            <p>There will always be a cross in the centre of the screen, and the <span className='targetTypeRef'>target</span>s will appear just above or below the cross.</p>
+            <center>
+              <table id="instructionPageTable">
+                <tbody>
+                <tr>
+                <td><img src={ArrowLeft} alt="left Arrow" className="target" /><img src={ArrowLeft} alt="left Arrow" className="target" /><img src={ArrowLeft} alt="left Arrow" className="target" /><img src={ArrowLeft} alt="left Arrow" className="target" /></td>
+                  <td className="spacing"></td>
+                </tr>
+                <tr>
+                  <td className="spacing"></td>
+                  <td className="spacing"></td>
+                  <td className="spacing"></td>
+                </tr>
+                <tr>
+                  <td><img src={Plus} alt="plus sign" className="cue" /></td>
+                  <td className="spacing">OR</td>
+                  <td><img src={Plus} alt="plus sign" className="cue" /></td>
+                </tr>
+                <tr>
+                  <td className="spacing"></td>
+                  <td className="spacing"></td>
+                  <td className="spacing"></td>
+                </tr>
+                <tr>
+                  <td className="spacing"></td>
+                  <td className="spacing"></td>
+                  <td><img src={ArrowLeft} alt="left Arrow" /><img src={ArrowLeft} alt="left Arrow" /><img src={ArrowLeft} alt="left Arrow" /><img src={ArrowLeft} alt="left Arrow" /><img src={ArrowLeft} alt="left Arrow" /></td>
+                </tr>
+                </tbody>
+              </table>
+            </center>
+            <p>Please try to keep your eyes fixed on the cross during the test, rather than moving them to look at the <u><span className='targetTypeRef'>target</span>s</u>.</p>
+            <input className="previousButton" type="button" name="previous" value="Previous" onClick={this.lastInstruction} />
+            <input className="nextButton" type="button" name="next" value="Next" onClick={this.nextInstruction} />
+          </div>
+          )
+        }
+        case 3:{
+          return(
+            <div id="instructionPage3" className="instructionsPage">
+            <p>Sometimes, one or more asterisks <img src={Star} className="cue imageintext"style={{ resizeMode: "cover",height: 35,width: 35 }} /> will appear shortly before the <span class='targetTypeRef'>target</span>s.</p>
+            <p>When they are presented, the asterisks always appear exactly one half second before the <span class='targetTypeRef'>target</span>s.</p>
+            <p>If only one asterisk appears, and it is above or below the cross, it also tells you the location in which the <span class='targetTypeRef'>target</span>s will appear.</p>
+            <input className="previousButton" type="button" name="previous" value="Previous" onClick={this.lastInstruction} />
+            <input className="nextButton" type="button" name="next" value="Next"  onClick={this.nextInstruction} />
+          </div>
+          )
+        }
+      }
+
     }else{
       return (
         <div className="App">
